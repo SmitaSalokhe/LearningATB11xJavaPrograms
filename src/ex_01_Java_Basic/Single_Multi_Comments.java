7e29c4114da14aee9564dab36c7e4e17667f6b04
Smita <sdhere@sveltoz.com>
2025-03-11 13:24:49 +0530

    6 March 2025 My name

---
+++ src/ex_01_Java_Basic/Single_Multi_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Single_Multi_Comments
+{
+    public static void main(String[] args) {
+
+        System.out.println("How to use comments- ");
+        System.out.println("//This is single line comment");
+        System.out.println("/*This is single line comment, This is single line comment*/");
+
+    }
+}
