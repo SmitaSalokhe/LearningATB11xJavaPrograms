ae58646e494d4ce435f4b736816ed373ef39ee70
Smita <sdhere@sveltoz.com>
2025-03-11 18:46:47 +0530

    10 March 2025 All tasks from 4th march 2025 are properly done

---
+++ src/ex_01_Java_Basic/L008_Printf.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class L008_Printf
+{
+    public static void main(String[] args) {
+        int a= 10;
+        //d% = byte, short, int, long,
+        //s% = short
+        //f% = float, double
+        //b% = boolean
+        System.out.printf("Your variable is %d", a);
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab007_Boolean.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Boolean
+{
+    public static void main(String[] args) {
+        boolean AreYouLeavingInIndia= true;
+
+        int a=10;
+        int b=20;
+        int result;
+
+        System.out.println("a is grater than b ? "+ (a>b));
+        System.out.println("b is grater than a ? "+ (b>a));
+    }
+}
